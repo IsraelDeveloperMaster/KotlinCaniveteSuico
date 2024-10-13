63e81f0cbdf0c496edea0b9226814f1396bd4adc
Israel <dev@developermaster.net>
2024-10-13 02:06:21 +0200

    googleMap Simples

--- app/src/main/java/net/developermaster/kotlincanivetesuico/ui/googleMap/googleMapSimples/FragmentGoogleMapSimples.kt
+++ app/src/main/java/net/developermaster/kotlincanivetesuico/ui/googleMap/googleMapSimples/FragmentGoogleMapSimples.kt
@@ -1,7 +1,6 @@
 package net.developermaster.kotlincanivetesuico.ui.googleMap.googleMapSimples
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -24,7 +23,117 @@ import net.developermaster.kotlincanivetesuico.utils.codigos.modelCodigos
 
 class FragmentGoogleMapSimples : Fragment() , OnMapReadyCallback {
 
-    private lateinit var map: GoogleMap
+    //////// GOOGLE MAP ////////
+
+    //todo variavel do googleMap
+    private lateinit var googleMap: GoogleMap
+
+    //todo configuracao de googleMap aqui
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+        this.googleMap.uiSettings.isZoomControlsEnabled = true
+        criandoMarcadores()
+        habilitandoLocalizacao()
+    }
+
+    //todo cria os marcadores
+    private fun criandoMarcadores() {
+
+        //todo marcador
+        val lugarFavorito = LatLng(28.044195, -16.5363842)
+        googleMap.addMarker(MarkerOptions().position(lugarFavorito).title("Mi playa favorita!"))
+
+        //todo animacao da camera
+        googleMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(lugarFavorito, 10f),
+            4000,
+            null
+        )
+    }
+
+    //todo verifica se tem permissao para acessar a localização
+    private fun isPermissionsGranted() = ContextCompat.checkSelfPermission(
+        requireContext(),
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PermissionChecker.PERMISSION_GRANTED
+
+    //todo habilita a localização no mapa
+    private fun habilitandoLocalizacao() {
+        if (!::googleMap.isInitialized) return
+        if (isPermissionsGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            googleMap.isMyLocationEnabled = true
+        } else {
+
+            requestLocationPermission()
+        }
+    }
+
+    //todo verifica se tem permissao para acessar a localização
+    companion object {
+        const val REQUEST_CODE_LOCATION = 0
+    }
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(requireContext(), "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
+        }
+    }
+
+    //todo verifica se tem permissao para acessar a localização
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                googleMap.isMyLocationEnabled = true
+            }else{
+                Toast.makeText(requireContext(), "Para activar la localización ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+        }
+    }
+
+
+    //////// GOOGLE MAP ////////
 
     //todo instancia de classe onde estao os codigos e xml
     val dados = modelCodigos()
@@ -76,68 +185,15 @@ class FragmentGoogleMapSimples : Fragment() , OnMapReadyCallback {
         _binding = null
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        map.uiSettings.isZoomControlsEnabled = true
-        criandoMarcadores()
-        enableMyLocation()
-    }
 
-    private fun criandoMarcadores() {
 
-        val favoritePlace = LatLng(28.044195, -16.5363842)
-        map.addMarker(MarkerOptions().position(favoritePlace).title("Mi playa favorita!"))
-        map.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(favoritePlace, 18f),
-            4000,
-            null
-        )
-    }
 
-    private fun isPermissionsGranted() = ContextCompat.checkSelfPermission(
-        requireContext(),
-        Manifest.permission.ACCESS_FINE_LOCATION
-    ) == PermissionChecker.PERMISSION_GRANTED
 
-    @SuppressLint("MissingPermission")
-    private fun enableMyLocation() {
-        if (!::map.isInitialized) return
-        if (isPermissionsGranted()) {
-            map.isMyLocationEnabled = true
-        } else {
 
-            requestLocationPermission()
-        }
-    }
 
-    companion object {
-        const val REQUEST_CODE_LOCATION = 0
-    }
-    private fun requestLocationPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),
-                Manifest.permission.ACCESS_FINE_LOCATION)) {
-            Toast.makeText(requireContext(), "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
-        } else {
-            ActivityCompat.requestPermissions(requireActivity(),
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
-        }
-    }
 
-    @SuppressLint("MissingPermission")
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when(requestCode){
-            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED){
-                map.isMyLocationEnabled = true
-            }else{
-                Toast.makeText(requireContext(), "Para activar la localización ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
-            }
-            else -> {}
-        }
-    }
+
+
 
 }
 

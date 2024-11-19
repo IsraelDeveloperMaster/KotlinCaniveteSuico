48994d47d3244a9b612bdeb4808f5f907ad1b0ea
Israel Santos <dev@developermaster.net>
2024-11-19 01:23:19 +0100

    ServiceBackGround Simples CoroutineScope

--- app/build/tmp/kapt3/stubs/debug/net/developermaster/kotlincanivetesuico/ui/servicesBackGround/classes/ServicesClass.java
+++ app/build/tmp/kapt3/stubs/debug/net/developermaster/kotlincanivetesuico/ui/servicesBackGround/classes/ServicesClass.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
-import kotlinx.coroutines.GlobalScope;
+import kotlinx.coroutines.Dispatchers;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000(\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0005\u0018\u00002\u00020\u0001:\u0001\u000fB\u0005\u00a2\u0006\u0002\u0010\u0002J\u0014\u0010\u0003\u001a\u0004\u0018\u00010\u00042\b\u0010\u0005\u001a\u0004\u0018\u00010\u0006H\u0016J\b\u0010\u0007\u001a\u00020\bH\u0016J\b\u0010\t\u001a\u00020\bH\u0016J\"\u0010\n\u001a\u00020\u000b2\b\u0010\f\u001a\u0004\u0018\u00010\u00062\u0006\u0010\r\u001a\u00020\u000b2\u0006\u0010\u000e\u001a\u00020\u000bH\u0016\u00a8\u0006\u0010"}, d2 = {"Lnet/developermaster/kotlincanivetesuico/ui/servicesBackGround/classes/ServicesClass;", "Landroid/app/Service;", "()V", "onBind", "Landroid/os/IBinder;", "servicos", "Landroid/content/Intent;", "onCreate", "", "onDestroy", "onStartCommand", "", "intent", "flags", "startId", "MinhaThread", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000.\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0005\u0018\u00002\u00020\u0001:\u0001\u0011B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0014\u0010\u0005\u001a\u0004\u0018\u00010\u00062\b\u0010\u0007\u001a\u0004\u0018\u00010\bH\u0016J\b\u0010\t\u001a\u00020\nH\u0016J\b\u0010\u000b\u001a\u00020\nH\u0016J\"\u0010\f\u001a\u00020\r2\b\u0010\u000e\u001a\u0004\u0018\u00010\b2\u0006\u0010\u000f\u001a\u00020\r2\u0006\u0010\u0010\u001a\u00020\rH\u0016R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0012"}, d2 = {"Lnet/developermaster/kotlincanivetesuico/ui/servicesBackGround/classes/ServicesClass;", "Landroid/app/Service;", "()V", "coroutine", "Lkotlinx/coroutines/CoroutineScope;", "onBind", "Landroid/os/IBinder;", "servicos", "Landroid/content/Intent;", "onCreate", "", "onDestroy", "onStartCommand", "", "intent", "flags", "startId", "MinhaThread", "app_debug"})
 public final class ServicesClass extends android.app.Service {
+    @org.jetbrains.annotations.NotNull
+    private final kotlinx.coroutines.CoroutineScope coroutine = null;
     
     public ServicesClass() {
         super();
